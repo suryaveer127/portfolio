c2c930bd71c1e2dedae61bf05edafedcad305bc8
suryaveer127 <120268594+suryaveer127@users.noreply.github.com>
2024-07-25 12:56:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop >20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+}
+
+
